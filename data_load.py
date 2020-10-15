e17a0f552d6df10ac2ec4977b3613f9c62616604
ohtack <ohtack1234@gmail.com>
2020-10-15 18:47:34 -0500

    add #1-1 & #1-2

--- data_load.py
+++ data_load.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import csv
 
-file = "/Users/hyungtaegoh/Desktop/PythonWorkspace/Project/ARIMA_implementation/TESLA.csv"
+file = "/Users/hyungtaegoh/Desktop/PythonWorkspace/Project/ARIMA/TESLA.csv"
 data = pd.read_csv(file)
 data.head()
